0d88f82ffcbb376bee2967253b7624480eb666f4
Arsen290 <arsen290@ukr.net>
2023-09-19 17:06:02 +0200

    Implement the Customer class

--- src/main/java/feg/assigment/app/Customer.java
+++ src/main/java/feg/assigment/app/Customer.java
@@ -1,4 +1,25 @@
 package feg.assigment.app;
 
 public class Customer {
+    private String name;
+    private double revenue;
+    public Customer (String name,double revenue){
+            this.name=name;
+            this.revenue=revenue;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
 }
